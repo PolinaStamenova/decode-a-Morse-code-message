13e1eff8c01171e567a5a506f1816dd47ac1fbaa
Akasha <Akasha.Rojee@gmail.com>
2021-10-01 18:14:04 +0400

    Create decode_word and decode_sentence

--- index.rb
+++ index.rb
@@ -8,3 +8,12 @@ end
 def decode_char(char)
   morse.key char
 end
+
+def decode_word(word)
+  word.split.map { |char| decode_char char }.join
+end
+
+def decode_sentence(morse_sentences)
+  morse_sentences.split('   ').map { |sentence| decode_word sentence }.join(' ')
+end
+puts decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.  /  .-. ..- -... .. . ...')
